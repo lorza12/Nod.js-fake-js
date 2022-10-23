dc178eb5a867a9d25df5ab4c20e7cff51eb0c579
juan_lorza <lorza112@hotmail.com>
2022-10-23 14:48:25 -0500

    nuevo commit

---
+++ faker.js
@@ -0,0 +1,18 @@
+import { faker } from '@faker-js/faker'
+import fs from 'fs';
+
+function fakeNames(Numberoftimes){
+  let string = ""
+    for(let i=0; i< Numberoftimes; i++){
+      string += (faker.name.firstName() + "\n")
+    }
+  return string
+}
+
+const names = fakeNames(1000)
+
+fs.writeFile("names.txt", names, err => {
+    if (err) {
+      console.error(err);
+    }
+  });
